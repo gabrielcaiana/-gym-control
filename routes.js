e17d9f8af27108ad51eee39951fd4c605fe79d57
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-03-09 12:44:58 -0300

    criando validaçao de form no back-end

--- routes.js
+++ routes.js
@@ -10,7 +10,19 @@ routes.get('/instructors', function(req, res){
 })
 
 routes.post('/instructors', function(req, res){
-    return res.send('Recebido')
+    //req.body
+    // retorna = { "avatar_url": "http://www.test.com.br", "name": "Gabriel", "birth": "2019-12-01", "gender": "M", "services": "rqr" }
+
+    //Keys retorna = [ "avatar_url", "name", "birth", "gender", "services" ]
+    const keys = Object.keys(req.body) // retorna um array com as chaves do objeto
+    
+    for(key of keys) {
+        // A linha abaixo é a mesma coisa que req.body.avatar_url ou qualquer coisa chave.
+        if (req.body[key] == "") {
+            return res.send("Please, fill all fields")
+        }
+    }
+    return res.send(req.body)
 })
 
 routes.get('/instructors/create', function(req, res){
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const routes = require('./routes')
 
 const server = express()
 
+server.use(express.urlencoded({ extended: true })) // linha responsável por fazer funcionar o req.body
 server.use(express.static('public'))
 server.use(routes)
 
