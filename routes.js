d02c2267f7b721de0ded09703f91b1f72e0f8b52
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-03-09 12:55:28 -0300

    create file instructors

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const routes = express.Router()
+const instructors = require('./instructors')
 
 routes.get('/', function(req, res){
     return res.redirect('/instructors')
@@ -9,21 +10,8 @@ routes.get('/instructors', function(req, res){
     return res.render('instructors/index')
 })
 
-routes.post('/instructors', function(req, res){
-    //req.body
-    // retorna = { "avatar_url": "http://www.test.com.br", "name": "Gabriel", "birth": "2019-12-01", "gender": "M", "services": "rqr" }
+routes.post('/instructors', instructors.post )
 
-    //Keys retorna = [ "avatar_url", "name", "birth", "gender", "services" ]
-    const keys = Object.keys(req.body) // retorna um array com as chaves do objeto
-    
-    for(key of keys) {
-        // A linha abaixo é a mesma coisa que req.body.avatar_url ou qualquer coisa chave.
-        if (req.body[key] == "") {
-            return res.send("Please, fill all fields")
-        }
-    }
-    return res.send(req.body)
-})
 
 routes.get('/instructors/create', function(req, res){
     return res.render('instructors/create')
