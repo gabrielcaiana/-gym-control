2770ff0c66596a02dd659012a1ffc16f7db01fac
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-03-16 08:31:12 -0300

    criando e utilizando lógica de idade

--- instructors.js
+++ instructors.js
@@ -1,45 +1,46 @@
 const fs = require('fs')
-const data = require("./data.json") // importando json data 
+const data = require("./data.json") // importando json data
+const { age } = require('./utils')
 
 //show
-exports.show = function(req, res) {
+exports.show = function (req, res) {
     const { id } = req.params
-    
-    const foundInstructor = data.instructors.find(function(instructor) {
+
+    const foundInstructor = data.instructors.find(function (instructor) {
         return instructor.id == id
     })
 
-    if(!foundInstructor) return res.send('Instructor not found!')
+    if (!foundInstructor) return res.send('Instructor not found!')
 
     //conceito spread
     const instructor = {
         ...foundInstructor, // coloco o objeto inteiro dentro da variavel instructor
-        age: "", // vou sebrescrever os valores 
+        age: age(foundInstructor.birth), // vou sebrescrever os valores 
         services: foundInstructor.services.split(","), // vou sebrescrever os valores
         created_At: "" // vou sebrescrever os valores
     }
 
-    return res.render('instructors/show', {instructor})
+    return res.render('instructors/show', { instructor })
 }
 //create
-exports.post = function(req, res){
+exports.post = function (req, res) {
     //req.body
     // retorna = { "avatar_url": "http://www.test.com.br", "name": "Gabriel", "birth": "2019-12-01", "gender": "M", "services": "rqr" }
 
     //Keys retorna = [ "avatar_url", "name", "birth", "gender", "services" ]
     const keys = Object.keys(req.body) // retorna um array com as chaves do objeto
-    
-    for(key of keys) {
+
+    for (key of keys) {
         // A linha abaixo é a mesma coisa que req.body.avatar_url ou qualquer coisa chave.
         if (req.body[key] == "") {
-            return res.send("Please, fill all fields")  
+            return res.send("Please, fill all fields")
         }
     }
 
-    let {avatar_url, birth, name, services, gender} = req.body
+    let { avatar_url, birth, name, services, gender } = req.body
 
     birth = Date.parse(birth) // Trabalhando com data no js
-    const created_At =   Date.now() // Trabalhando com data no js
+    const created_At = Date.now() // Trabalhando com data no js
     const id = Number(data.instructors.length + 1)
 
     data.instructors.push({
@@ -47,13 +48,13 @@ exports.post = function(req, res){
         name,
         avatar_url,
         birth,
-        gender,   
+        gender,
         services,
         created_At
     }) // utilizando o arquivo json data - adicionando o req.body no array
 
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if(err) {
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+        if (err) {
             return res.send('Write file error')
         }
         return res.redirect('/instructors')
