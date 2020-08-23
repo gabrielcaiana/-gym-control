9e6ead3ea87b1b378b8bf3eec3932f36dc441e3f
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-08-22 21:27:52 -0300

    refatorando estrutura de pastas

---
+++ src/app/controllers/instructors.js
@@ -0,0 +1,40 @@
+const { age, date } = require("../../lib/utils");
+
+modules.exports = {
+  index(req, res) {
+    return res.render("instructors/index");
+  },
+  create(req, res) {
+    return res.render("instructors/create");
+  },
+  post(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields");
+      }
+    }
+    return;
+  },
+  show(req, res) {
+    return;
+  },
+  edit(req, res) {
+    return;
+  },
+  put(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields");
+      }
+    }
+
+    return;
+  },
+  delete(req, res) {
+    return;
+  },
+};
